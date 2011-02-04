d511585cf382c7decbf3a04d5523fbdba1b9ec46
J.P. Lacerda <jpl@codethink.co.uk>
2011-02-04 15:14:43 +0000

    Organized files

---
+++ functions/user.php
@@ -0,0 +1,37 @@
+<?php
+class user {
+
+	private $firstName;
+	private $lastName;
+	private $password;
+	private $email;
+	private $rfid;
+
+	public function __construct($firstName, $lastName, $password, 
+				    $email, $rfid) {
+		$this->firstName = firstName;
+		$this->lastName = lastName;
+		$this->password = password;
+		$this->email = email;
+		$this->rfid = rfid;
+	}
+
+	public function addRoom($id, $name, $description) {
+
+		// create a new room with given parameters
+
+	}
+
+	public rooms[] function getRooms() {
+
+		// return rooms which owner set to this.rfid
+
+	}
+
+	public preferences[] function getPreferences() {
+
+		// get preferences (is_user) associated with his RFID. 
+
+	}
+}
+?>
